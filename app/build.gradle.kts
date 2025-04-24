428f120567a06b34ace4ab6a4d681c116653ccfb
shixwase <karennatalie922@gmail.com>
2025-04-24 14:03:01 +0200

    API pt1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,12 @@ dependencies {
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
 
+    //Retrofit
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/moviedb2025/database/MoviesRepository.kt
@@ -0,0 +1,19 @@
+package com.example.moviedb2025.database
+
+import com.example.moviedb2025.models.MovieResponse
+import com.example.moviedb2025.network.MovieDBApiService
+
+interface MoviesRepository {
+    suspend fun getPopularMovies(): MovieResponse
+    suspend fun getTopRatedMovies(): MovieResponse
+}
+
+class NetworkMoviesRepository(private val apiService: MovieDBApiService): MoviesRepository {
+    override suspend fun getPopularMovies(): MovieResponse {
+        return apiService.getPopularMovies()
+    }
+
+    override suspend fun getTopRatedMovies(): MovieResponse {
+        return apiService.getTopRatedMovies()
+    }
+}
